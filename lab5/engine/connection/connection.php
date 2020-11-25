57d322d3bc3905f9632b727ca6a81a52e7ee8882
slaash1 <71807098+slaash1@users.noreply.github.com>
2020-11-25 13:03:51 +0500

    Add engine

---
+++ lab5/engine/connection/connection.php
@@ -0,0 +1,6 @@
+<?php
+$host = 'localhost';
+$user = 'f0478433_root';
+$password = 'root';
+$database = 'f0478433_db';
+?>
---
+++ lab5/engine/page/title.php
@@ -0,0 +1,9 @@
+<!doctype html>
+<html>
+	<head>
+        <?require_once 'head.php';?>
+        <title>
+            Айдар Гиниятуллин Лабораторная работа №5
+        </title>
+	</head>
+</html>
